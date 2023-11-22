aa9744a379b21b87f759b62923207e5ad67e4d92
park31821 <74476122+park31821@users.noreply.github.com>
2023-11-22 22:47:01 +1000

    Update api.js

--- stock_web_application/src/api.js
+++ stock_web_application/src/api.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://www.alphavantage.co/query",
   params:{
-      apikey: "4N7PWZO4K5KEQ83G"
+      apikey: "Placeholder"
   }
 });
 
 const api2 = axios.create({
     baseURL: "https://financialmodelingprep.com/api/v3",
     params:{
-        apikey:"1d1a86fa49e866279effef61ce0990fd"
+        apikey: "Placeholder"
     }
 });
 
